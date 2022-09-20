0e994d8805340c953998c4e53c274912d0516e4d
Samuel Majerus <ladolego@gmail.com>
2022-09-20 14:53:21 -0700

    Adds result of code-along work for Lessons 15-16.

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,11 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
+using System.Collections.Generic;
+using System;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
+
+    public void Dispose(){
+      Item.ClearAll();
+    }
 
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
@@ -44,5 +50,17 @@ namespace ToDoList.Tests
       Assert.AreEqual(updatedDescription, result);
     }
     
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      // Arrange
+      List<Item> newList = new List<Item> { };
+
+      // Act
+      List<Item> result = Item.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,13 +1,27 @@
+using System.Collections.Generic; 
+
 namespace ToDoList.Models
 {
   public class Item
   {
 
     public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> {}; 
 
     public Item(string description)
     {
       Description = description;
+      _instances.Add(this); //Adds Item to '_instances'
+    }
+
+    public static List<Item> GetAll()  //'Getter' method
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
 
   }
