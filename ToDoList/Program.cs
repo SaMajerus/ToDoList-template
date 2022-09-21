1eb970b19f623c4c9e1ee497e878bd260cebbb5d
Samuel Majerus <ladolego@gmail.com>
2022-09-21 13:53:41 -0700

    Adds working 'View (list)' and 'End' commands. (The latter one is necessary as we use a while loop to make sure the process doesn't end after each command's operation is finished.)     TLDR:  MVP (Minimum-Viable-Product) complete!
    Co-authored-by: Shaniza Lingle <shanizalingle@gmail.com>
    Co-authored-by: Trevor Hunter <trevorh8700@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,4 +1,3 @@
-/*     UI Logic     */ 
 using System;
 using System.Collections.Generic;
 using ToDoList.Models;
@@ -7,40 +6,43 @@ namespace ToDoList
 {
   public class Program
   {
-    // public string CmdInput { get; set; }
-    
-    // public void CmdProcessor(string input)
-    // {
-    //   CmdInput = input;
-    // }
     
     public static void Main()
     {
-      Console.WriteLine("Welcome to the To Do List");
-      Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+      welcome(); 
       
-      //Save user input to string variable, run through branch logic to make sure they entered a valid command phrase.
-      string userInput = Console.ReadLine().ToLower();
-      
-      if (userInput == "add") {
-        Console.WriteLine("Please enter the description for the new item.");
-        string item = Console.ReadLine();
-        Item newItem = new Item(item);  //Creates new item, which is automatically aded to the list. 
+      int loop = 0;
+      bool repeatLoop = true;
+      while (repeatLoop)
+      {
+        Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View) or end? (End)");
+        string userInput = Console.ReadLine().ToLower();
+        
+        if (userInput == "add") {
+          Console.WriteLine("Please enter the description for the new item.");
+          string item = Console.ReadLine();
+          Item newItem = new Item(item); 
 
-      } else if (userInput == "view"){
-        Console.WriteLine("Your list: ");
+        } else if (userInput == "view"){
+          Console.WriteLine("Your list: ");
+          List<Item> currentList = Item.GetAll();
 
-        // display list
-        List<Item> currentList = new List<Item> { }; 
-        currentList = Item.GetAll(); 
+          foreach(Item i in currentList) 
+          {
+            loop += 1;
+            Console.WriteLine(loop + ") " + i.Description + \n);
+          }
 
-        foreach(Item i in currentList) 
-        {
-          Console.WriteLine(i.Description);
+        } else if (userInput == "end"){
+          repeatLoop = false;
+
+        } else {
+          Console.WriteLine("This is not a valid command, please try again");
         }
+      }
 
-      } else {
-        Console.WriteLine("This is not a valid command, please try again");
+      void welcome(){
+        Console.WriteLine("Welcome to the To Do List");       
       }
     }
   }
