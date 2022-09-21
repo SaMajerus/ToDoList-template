b2cad94c5eb6bad423103fdb3f60ab7cbe8db111
Samuel Majerus <ladolego@gmail.com>
2022-09-21 08:52:51 -0700

    Initial commit of Program.cs.  (This commit and the ones that follow are done in class Wed, 9-21-2022, with pairs)

---
+++ ToDoList/Program.cs
@@ -0,0 +1,34 @@
+/*     UI Logic     */ 
+
+
+
+/* 
+using System;
+using System.Collections.Generic;
+using Triangle;
+
+namespace Triangle
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Enter the length for Side 1:  ");
+      string stringS1 = Console.ReadLine();
+      int s1 = int.Parse(stringS1);
+
+      Console.WriteLine("Enter the length for Side 2:  ");
+      string stringS2 = Console.ReadLine();
+      int s2 = int.Parse(stringS2);
+
+      Console.WriteLine("Enter the length for Side 3:  ");
+      string stringS3 = Console.ReadLine();
+      int s3 = int.Parse(stringS3);
+
+      Triangle testThis = new Triangle(s1, s2, s3);
+      Console.WriteLine(testThis.Classify());
+      
+    }
+  }
+}
+*/ 
