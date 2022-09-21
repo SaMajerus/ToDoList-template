ef1bcc4f7380ebd62e653e99e7b9b2d9192d0913
Samuel Majerus <ladolego@gmail.com>
2022-09-21 12:14:04 -0700

    Adds the first iteration of a 'view' command for the user that is still very much a WIP.  (Currently, if an Item is added to the List in one 'dotnet run' session, the updated '_instances' List doesn't carry over into subsequent sessions too. As a result, the View command doesn't work as intended because there's nothing to retrieve and/or print out, since the '_instances' List is effectively reset by one session stopping and another starting.)      [WIP:  Implement a static(?) 'List<Item>' object to ensure the carryover of each update to that Object.  (If the List object doesn't work out for us, we'll probably switch to using an Array.)]
    Co-authored-by: Shaniza Lingle <shanizalingle@gmail.com>
    Co-authored-by: Trevor Hunter <trevorh8700@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -7,12 +7,12 @@ namespace ToDoList
 {
   public class Program
   {
-    public string CmdInput { get; set; }
+    // public string CmdInput { get; set; }
     
-    public void CmdProcessor(string input)
-    {
-      CmdInput = input;
-    }
+    // public void CmdProcessor(string input)
+    // {
+    //   CmdInput = input;
+    // }
     
     public static void Main()
     {
@@ -25,15 +25,20 @@ namespace ToDoList
       if (userInput == "add") {
         Console.WriteLine("Please enter the description for the new item.");
         string item = Console.ReadLine();
-        
-        // add to list
-        Item newItem = new Item(item); 
+        Item newItem = new Item(item);  //Creates new item, which is automatically aded to the list. 
 
       } else if (userInput == "view"){
         Console.WriteLine("Your list: ");
 
         // display list
-        /*Code goes here*/ 
+        List<Item> currentList = new List<Item> { }; 
+        currentList = Item.GetAll(); 
+
+        foreach(Item i in currentList) 
+        {
+          Console.WriteLine(i.Description);
+        }
+
       } else {
         Console.WriteLine("This is not a valid command, please try again");
       }
